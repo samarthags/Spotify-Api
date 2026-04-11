5a78b4d27a5de83ff03dd9fb3ca5b481639b7cb8
Samartha Gs <119740787+samarthags@users.noreply.github.com>
2026-04-11 14:02:35 +0530

    Delete save.js

--- pages/api/save.js
+++
@@ -1,7 +0,0 @@
-import { saveData } from "../../lib/store";
-
-export default function handler(req, res) {
-  const { id, userData } = req.body;
-  saveData(id, userData);
-  res.json({ ok: true });
-}
