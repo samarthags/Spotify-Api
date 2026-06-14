60a9774ee75ba937ad643f1940b59971ac6ae672
Samartha Gs <119740787+samarthags@users.noreply.github.com>
2026-06-14 20:04:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.scdn.co', 'mosaic.scdn.co'],
+    domains: ['i.scdn.co', 'mosaic.scdn.co', 'lineup-images.scdn.co', 'image-cdn-ak.spotifycdn.com', 'image-cdn-fa.spotifycdn.com'],
   },
 }
+
 module.exports = nextConfig
